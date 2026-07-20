561a2b31b4cd3a0c62a79559128f4c9a6d1718e7
Hunteraulo1 <hunteraulo@pm.me>
2026-07-20 14:32:02 +0200

    :package: add tailwind-variant and fix svelte config

---
+++ svelte.config.ts
@@ -0,0 +1,23 @@
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const config = {
+	preprocess: vitePreprocess(),
+
+	compilerOptions: {
+		runes: ({ filename }: { filename: string }) =>
+			filename.split(/[/\\]/).includes('node_modules')
+				? undefined
+				: true,
+	},
+
+	kit: {
+		adapter: adapter(),
+
+		alias: {
+			$features: './src/features',
+		},
+	},
+};
+
+export default config;
